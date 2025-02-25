30820edefe1317fd8d02e5b116ff3af3be3de518
NalenSrin123 <161912473+NalenSrin123@users.noreply.github.com>
2025-02-25 10:14:23 +0700

    Delete product

--- DOM/modal.js
+++ DOM/modal.js
@@ -1,5 +1,11 @@
 const addUser=document.querySelector('#addUser');
 const modal=document.querySelector('.modals');
+const id=document.querySelector('#id');
+const username=document.querySelector('#username');
+const email=document.querySelector('#email');
+const passowrd=document.querySelector('#password');
+const profile=document.querySelector('#profile');
+const tbody=document.querySelector('#tbody')
 addUser.addEventListener('click',function(){
     modal.style.display="block"
 })
@@ -7,3 +13,35 @@ const cancel=document.querySelector('#cancel');
 cancel.addEventListener("click",function(){
     modal.style.display='none';
 }) 
+function saveProduct(){
+  const btnSave=document.querySelector('#btnSave');
+  btnSave.addEventListener('click',function(){
+    modal.style.display='none';
+     tbody.innerHTML+=`
+        <tr>
+            <td>${id.value}</td>
+            <td>${username.value}</td>
+            <td>${email.value}</td>
+            <td>${passowrd.value}</td>
+            <td><img class="rounded-circle" width="80px" src="${URL.createObjectURL(profile.files[0])}" alt=""></td>
+            <td>
+                <button class="btn btn-success me-1">Edit</button>
+                <button class="btn btn-danger" id="btnDelete" onclick="deleteProduct(this)">Delete</button>
+            </td>
+        </tr>
+     `;
+     id.value='';
+     username.value='';
+     email.value='';
+     passowrd.value='';
+     profile.value='';
+  })  
+}
+saveProduct();
+function deleteProduct(rowIndex){
+    const row=rowIndex.parentElement.parentElement;
+    if(confirm("Are you sure to delete this user?")){
+        row.remove()
+    }
+    
+}
